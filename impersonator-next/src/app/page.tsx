fa94fddbaee2e9494a727e01f984411d982b4476
Atulya Weise <30200282+atultw@users.noreply.github.com>
2025-03-01 18:56:53 -0800

    Co-authored-by: Jesse.net <JesseDoesStuff@users.noreply.github.com>

--- impersonator-next/src/app/page.tsx
+++ impersonator-next/src/app/page.tsx
@@ -1,9 +1,8 @@
+"use client"
 import * as React from 'react';
 import { AudioRecorder } from 'react-audio-voice-recorder';
+import { Button } from "@/components/ui/button"
 
-function cosineDistance() {
-
-}
 
 export default function App() {
 
@@ -11,53 +10,35 @@ export default function App() {
     console.log("starting upload")
     const formData = new FormData();
     formData.append("file", blob, "file.webm");
-  
+
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://9d25-12-7-77-162.ngrok-free.app/", true);
-  
+
     xhr.onload = function () {
       if (xhr.status === 200) {
         console.log("Upload successful:", xhr.responseText);
-        setCurrentRecording(blob);
-        setCurrentVector(JSON.parse(xhr.responseText).embedding);
-      
       } else {
         console.error("Upload failed:", xhr.status, xhr.statusText);
       }
     };
-  
+
     xhr.onerror = function () {
       console.error("Request failed");
     };
-  
+
     xhr.send(formData);
   };
 
-  function onGameStart() {
-      setTargetPlayer(players[Math.floor(Math.random()*players.length)])
-      
-  }
-
-  function incrementCurrentImitator() {
-    if (currentImitatorIndex + 1 < players.length) {
-      setCurrentImitatorIndex(currentImitatorIndex + 1)
-    } else {
-      roundEnd();
-    }
-  }
-
-  function roundEnd() {
-    for ()
-  }
-
-  const [currentVector, setCurrentVector] = React.useState<any>();
-  const [currentRecording, setCurrentRecording] = React.useState<any>();
   const [players, setPlayers] = React.useState<any[]>([])
-  const [targetPlayer, setTargetPlayer] = React.useState<any>();
-  const [currentImitatorIndex, setCurrentImitatorIndex] = React.useState<any>(-1);
 
-  const [attempts, setAttempts] = React.useState(
-    [{
+  const [data, setData] = React.useState({
+    targetPlayer: {
+      wins: 2,
+      name: "Jesse",
+      mp3File: 10010101,
+      voiceVector: [1, 2, 3, 4, 5, 500]
+    },
+    attempts: [{
       attempter: {
         wins: 2,
         name: "Jesse",
@@ -67,7 +48,7 @@ export default function App() {
       attemptMp3: 1000101,
       attemptVector: [1, 2, 3, 4, 5, 500]
     }]
-  )
+  })
 
   const [inputValue, setInputValue] = React.useState('');
 
@@ -80,6 +61,7 @@ export default function App() {
   // Rank the comparisons by similarity and award the highest similarity by adding a win to that player object
   // Clear attempts array and shift targetPlayer to the next player in array
 
+
   const addAudioElement = (blob: Blob) => {
     const url = URL.createObjectURL(blob);
     const audio = document.createElement('audio');
@@ -90,44 +72,45 @@ export default function App() {
 
   return (
     <div>
-      <h1>
-        {
-          targetPlayer && currentImitatorIndex ? `${players[currentImitatorIndex].name}, imitate ${targetPlayer.name}` : ''
-        }
-      </h1>
-      <input type="text" placeholder='Username' onChange={onChangeHandler} value={inputValue} />
-      <button onClick={() => {
+
+      <input type="text" placeholder='Username' onChange={onChangeHandler} value={inputValue}></input>
+      <Button onClick={() => {
         let newPlayer = {
           wins: 0,
           name: inputValue,
-          mp3File: currentRecording,
-          voiceVector: currentVector,
-          previousPlayer: false
+          mp3File: null,
+          voiceVector: null
         }
         setPlayers([...players, newPlayer])
-
         console.log(newPlayer);
-      }}>Add user</button>
+      }}>Add Player</Button>
 
       <ul>
-        {players.map((player) => (
-          <li>{player.name}</li>
-        ))}        
+        {/* {players.map(player) => (
+          
+        )}         */}
       </ul>
 
-      <button onClick={onGameStart} >Start Game</button>
-      {currentImitatorIndex >= 0 ? <button>Submit/Next Player</button> : <></>}
-
-      <AudioRecorder 
-      onRecordingComplete={uploadAudio}
-      audioTrackConstraints={{
-        noiseSuppression: true,
-        echoCancellation: true,
-      }} 
-      downloadOnSavePress={false}
-      downloadFileExtension="mp3"
-      showVisualizer={true}
-    />
+      <AudioRecorder
+        onRecordingComplete={uploadAudio}
+        audioTrackConstraints={{
+          noiseSuppression: true,
+          echoCancellation: true,
+          // autoGainControl,
+          // channelCount,
+          // deviceId,
+          // groupId,
+          // sampleRate,
+          // sampleSize,
+        }}
+        onNotAllowedOrFound={(err) => console.table(err)}
+        downloadOnSavePress={true}
+        downloadFileExtension="webm"
+        mediaRecorderOptions={{
+          audioBitsPerSecond: 128000,
+        }}
+        showVisualizer={true}
+      />
       <br />
     </div>
   );
--- impersonator/src/index.tsx
+++ impersonator/src/index.tsx
@@ -1,41 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AudioRecorder } from 'react-audio-voice-recorder';
-
-const uploadAudio = (blob: any) => {
-  console.log("starting upload")
-  const formData = new FormData();
-  formData.append("file", blob, "file.webm");
-
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://e15c-12-7-77-162.ngrok-free.app/", true);
-
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log("Upload successful:", xhr.responseText);
-    } else {
-      console.error("Upload failed:", xhr.status, xhr.statusText);
-    }
-  };
-
-  xhr.onerror = function () {
-    console.error("Request failed");
-  };
-
-  xhr.send(formData);
-};
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
-    <AudioRecorder 
-      onRecordingComplete={uploadAudio}
-      audioTrackConstraints={{
-        noiseSuppression: true,
-        echoCancellation: true,
-      }} 
-      downloadOnSavePress={false}
-      downloadFileExtension="mp3"
-      showVisualizer={true}
-    />
+    <App/>
   </React.StrictMode>
 );
